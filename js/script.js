00ef61809667e6dd755838271f66404aec3195c8
Joe Hertig <joehertig94@gmail.com>
2021-09-09 14:36:57 -0600

    completed issues #7, #8, #10 and #14

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ var submitEl = document.querySelector("#buttonSubmit");
 var userInputName = document.querySelector("#name");
 var userInputAddress = document.querySelector("#address");
 var userInputZip = document.querySelector("#zip");
+var yesEl = document.querySelector("#radio-yes");
+var noEl = document.querySelector("#radio-no");
 var isDelEl = document.getElementById("isDeliveryId");
+
+// hide google search on load.
+document.getElementById("google-search").style.display = "none";
 
 // save to local storage
 localStorage.setItem("name", userInputName.value);
 localStorage.setItem("address", userInputAddress.value);
 localStorage.setItem("zip", userInputZip.value);
 
+// Delivery ASAP hidden
 isDelEl.onmouseover = function () {
   isDelEl.textContent = "DO YOU NEED 911?";
   isDelEl.setAttribute("Style", "color: tomato;");
@@ -18,7 +24,9 @@ isDelEl.onmouseout = function () {
   isDelEl.textContent = "Delivery ASAP?";
   isDelEl.setAttribute("style", "color: --secondary-color;");
 };
+// Delivery ASAP hidden end
 
+// sending sms function submitCallBack();
 var submitCallBack = function () {
   // fetch start
   fetch(
@@ -49,22 +57,48 @@ var submitCallBack = function () {
 
 // when submit is clicked function.
 submitEl.onclick = function () {
-  // e.preventDefault();
   // save to local storage
-  var randomEl = localStorage.getItem("name");
+  localStorage.getItem("name");
   localStorage.getItem("address");
   localStorage.getItem("zip");
-  console.log(randomEl);
+
   // If check yes, go to submitCallBack(); function
   if (document.getElementById("radio-yes").checked) {
-    alert("yes");
+    // Show/un-hide google search bar when yes is clicked
+    document.getElementById("google-search").style.display = "block";
+    // alert("yes");
     submitCallBack();
   } else {
-    alert("nothing to see here");
+    // Show/un-hide google search bar when no is clicked
+    document.getElementById("google-search").style.display = "block";
+
+    // alert("nothing to see here");
   }
 };
 
-var totalCategory = function () {};
+yesEl.onclick = function () {
+  // show category section since yes was clicked
+  document.getElementById("category").style.display = "block";
+  document.getElementById("pizza-input").style.display = "initial";
+  document.getElementById("pizza").style.display = "initial";
+  document.getElementById("burgers-input").style.display = "initial";
+  document.getElementById("burgers").style.display = "initial";
+  document.getElementById("ethnic-input").style.display = "initial";
+  document.getElementById("ethnic").style.display = "initial";
+};
+
+//
+noEl.onclick = function () {
+  // hide category section since no was clicked
+  document.getElementById("category").style.display = "none";
+  document.getElementById("pizza-input").style.display = "none";
+  document.getElementById("pizza").style.display = "none";
+  document.getElementById("burgers-input").style.display = "none";
+  document.getElementById("burgers").style.display = "none";
+  document.getElementById("ethnic-input").style.display = "none";
+  document.getElementById("ethnic").style.display = "none";
+};
+
 // // Category section
 // if (document.getElementById("pizza-input").checked) {
 //   alert("Pizza");
